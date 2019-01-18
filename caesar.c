1f4ec6318ed9f55e168b6d4851d1b00c9bd7b100
vishnucs50 <vishnucs50@users.noreply.github.com>
2019-01-18 21:35:51 +0000

    20190118T213437Z

--- caesar.c
+++ caesar.c
@@ -1,32 +1,42 @@
 #include<stdio.h>
-#include<cs50.h>
+#include<stdlib.h>
 #include<string.h>
+#include<cs50.h>
 #include<ctype.h>
-int main(int argc, string argv[]){
 
-
-    if(argc!=2){
-        printf("Enter 2 arguments \n");
-        return 0;
+int main (int argc, string argv[]){
+    char upper[26];
+    char lower[26];
+    int z=0;
+    //Index alphabets from A to Z
+    for( int l=65;l<=90;l++){
+        upper[z]=(char)l; //insert appropriate aplhabets into the array using ascii values.
+        z++;
     }
-    int key=atoi(argv[1]);
-    if(key<0){
-        printf("Enter valid key value \n ");
-        return 0;
+    z=0;
+    //Index alphabets from a to z
+    for (int l =97; l<=122;l++){
+        lower[z]=(char)l;
+        z++;
     }
-    string msg=get_string("Enter your msg: ");
-    for (int i=0, n=strlen(msg); i<n;i++){
-        if(isupper(msg[i])){
-            printf("%c",(msg[i]+key-65)%26 + 65);
-        }
-        else if(islower(msg[i])){
-            printf("%c",(msg[i]+key-97)%26+97);
-        }
-        else{
-            printf("%c",msg[i]);
-        }
+    //convert string argument into integer. KEY
+    int i = atoi(argv[1]);
+    string s = get_string("Enter Message: ");
 
-
+    for(int j=0; j<strlen(s);j++){
+        //check if input is alphabet
+        if(isalpha(s[j])){
+            //check if input is upper case
+            if(isupper(s[j])){
+                int n= ((int)s[j]-((int)upper[0])+i)%26;
+                printf("%c",upper[n]);
+            }
+            //check if input is lower case
+            if(islower(s[j])){
+                int n= ((int)s[j]-((int)lower[0])+i)%26;
+                printf("%c",lower[n]);
+            }
+        }
     }
     printf("\n");
 
